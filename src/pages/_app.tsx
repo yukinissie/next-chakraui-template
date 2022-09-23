8c74dd50a8d467ebf936a740a03c863bffe42f5e
yukinissie <bigtree2490@outlook.jp>
2022-09-24 05:33:28 +0900

    add eslint and prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,14 @@
-import Head from "next/head";
-import { ChakraProvider } from "@chakra-ui/react";
+import Head from 'next/head';
+import { ChakraProvider } from '@chakra-ui/react';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Next App</title>
-      </Head>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </>
-  );
-}
-
+const MyApp = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>Next App</title>
+    </Head>
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  </>
+);
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import { Box } from "@chakra-ui/react";
+import { Box } from '@chakra-ui/react';
 
-export default function Home() {
-  return (
-    <Box bg='tomato' w='100%' p={4} color='white'>
-      Hello Chakra
-    </Box>
-  );
-}
+const Home = () => (
+  <Box bg="tomato" w="100%" p={4} color="white">
+    Hello Chakra
+  </Box>
+);
+
+export default Home;
